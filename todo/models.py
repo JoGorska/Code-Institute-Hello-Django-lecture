fc874c2cf001d381feaf8b03c1ab07254eb5f9b4
Joanna Gorska <gorska919@gmail.com>
2021-12-03 11:14:24 +0000

    Updated templates so user can see the to do items

--- todo/models.py
+++ todo/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+
 class Item(models.Model):
+    """
+    creates table Item, that has 2 headers: name and done
+    """
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
 
