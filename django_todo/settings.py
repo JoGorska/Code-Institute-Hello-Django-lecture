5ab68ac4e36cd7068b69501c9b3ea027331e79fa
Joanna Gorska <gorska919@gmail.com>
2021-12-04 20:00:11 +0000

    Add heroku app to allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['to-do-list-in-django.herokuapp.com']
 
 
 # Application definition
