3b874f5484c80c5e3cb30f12987f591d588cd19f
Joanna Gorska <gorska919@gmail.com>
2021-12-04 13:10:12 +0000

    Add tests for Forms

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .forms import ItemForm
+
+
+class TestItemForm(TestCase):
+    def test_item_name_is_required(self):
+        form = ItemForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+    def test_done_field_is_not_required(self):
+        form = ItemForm({'name': 'Test todo item'})
+        self.assertTrue(form.is_valid())
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = ItemForm
+        self.assertEqual(form.Meta.fields, ['name', 'done'])
