19f3ade31b2369afc24139d8d7b3ed19ee1a212b
Joanna Gorska <gorska919@gmail.com>
2021-12-04 17:14:17 +0000

    Add test to acheve 100 percent coverage, created html coverage report

--- todo/tests_models.py
+++ todo/tests_models.py
@@ -5,3 +5,7 @@ class TestModels(TestCase):
     def test_done_default_to_false(self):
         item = Item.objects.create(name='Test todo Item')
         self.assertFalse(item.done)
+
+    def test_items_string_method_returns_name(self):
+            item = Item.objects.create(name='Test todo Item')
+            self.assertEqual(str(item), 'Test todo Item')
--- todo/tests_views.py
+++ todo/tests_views.py
@@ -13,7 +13,7 @@ class TestViews(TestCase):
         response = self.client.get('/add')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/add_item.html')
-    
+
     def test_get_edit_item_page(self):
         item = Item.objects.create(name='Test todo Item')
         response = self.client.get(f'/edit/{item.id}')
@@ -37,3 +37,10 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test todo Item')
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
