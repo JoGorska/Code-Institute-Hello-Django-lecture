5c32b404a126d383722880c97df2023315772ea9
Joanna Gorska <gorska919@gmail.com>
2021-12-04 15:21:50 +0000

    Add tests for views.py

---
+++ todo/tests_views.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+
+from .models import Item
+
+
+class TestViews(TestCase):
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
+
+    def test_get_add_item_page(self):
+        response = self.client.get('/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/add_item.html')
+    
+    def test_get_edit_item_page(self):
+        item = Item.objects.create(name='Test todo Item')
+        response = self.client.get(f'/edit/{item.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/edit_item.html')
+
+    def test_can_add_item(self):
+        response = self.client.post('/add', {'name': 'Test Add Item'})
+        self.assertRedirects(response, '/')
+
+    def test_can_delete_item(self):
+        item = Item.objects.create(name='Test todo Item')
+        response = self.client.get(f'/edit/{item.id}')
+        self.assertRedirects(response, '/')
+        existing_items = Item.objects.filter(id=item.id)
+        self.assertEqual(len(existing_items), 0)
