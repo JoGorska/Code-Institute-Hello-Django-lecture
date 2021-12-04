16d1682f1fbe516db4c9d48beee6eb73ca3208a6
Joanna Gorska <gorska919@gmail.com>
2021-12-04 10:53:53 +0000

    Add button and function to delete items from database

--- todo/views.py
+++ todo/views.py
@@ -51,3 +51,10 @@ def toggle_item(request, item_id):
     item.done = not item.done
     item.save()
     return redirect('get_todo_list')
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_todo_list')
+
