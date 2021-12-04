ef18a59d6cdbd232d13fe83c4f75fbf369b289b3
Joanna Gorska <gorska919@gmail.com>
2021-12-04 19:39:48 +0000

    Fix issue with secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+# to hide secret key
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c_6vsdvr(txfcg8h5d#jk&9b#-)c9ihzgfv!=)i74a0(nlrmh+'
+# secret key in env.py
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
