3338f84c6c2e5a59a498e92f14efafa60f3457ee
Joanna Gorska <gorska919@gmail.com>
2022-01-01 19:23:49 +0000

    Removed cloudinary storage from settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -56,10 +56,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
     "django_forms_bootstrap",
-    'cloudinary',
     'todo'
 ]
 
